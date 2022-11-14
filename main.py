b2e622d5a3d7cd0033e83030c1f1ce95baa41da1
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-11-14 16:08:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,40 +2,8 @@ import sys
 import pygame
 import random
 from pygame.math import Vector2
-#sys.path.insert(0, '.')
-#from classes import Fruit
-
-class Passageiro: # Classe cujos objetos representam os passageiros
-    def __init__(self):
-        self.x = random.randint(0, cn-1)
-        self.y = random.randint(0, cn-1)
-        self.pos = Vector2(self.x, self.y) 
-        # A posição é randomizada dentro dos limites da tela e guardada num vetor.
-
-    def desenhar_passageiro(self):
-        passageiro_rect = pygame.Rect(int(self.x*cs), int(self.y*cs), cs, cs)
-        pygame.draw.rect(screen, (111, 222, 142), passageiro_rect)
-        # O passageiro é renderizado como um bloco colorido
-        
-class Trem:
-    def __init__(self):
-        self.corpo = [Vector2(5,2), Vector2(4,2), Vector2(3,2)]
-        self.sentido = Vector2(1,0)
-        # O trem começa com três blocos numa posição definida, que compõem seu corpo, e com um sentido de movimanto também já definido
-    
-    def desenhar_trem(self):
-        for vagao in self.corpo:
-            vagao_rect = pygame.Rect(int(vagao.x*cs), int(vagao.y*cs), cs, cs)
-            pygame.draw.rect(screen, (111, 222, 142), vagao_rect)
-        # Cada vagão do trem é um bloco
-    
-    def mover_trem(self):
-        corpo_copia = self.corpo[:-1]
-        corpo_copia.insert(0,corpo_copia[0]+self.sentido)
-        self.corpo = corpo_copia[:]
-        # QUando o trem se move, o último vagão é eliminado e adiciona-se um vagão à frente dos outros(no começo da lista), que é uma cópia do primeiro vagão 
-        # mais uma vez o sentido no qual o trem se move
-        
+sys.path.insert(0, '.')
+from classes import Passageiro, Trem
 
 
 pygame.init()
@@ -43,8 +11,9 @@ cs = 16 #cell_size
 cn = 40 #cell_number
 screen = pygame.display.set_mode((cn*cs,cn*cs))
 clock = pygame.time.Clock()
-passageiro = Passageiro() # Cria um objeto da classe Passageiro
-trem = Trem() # Cria um objeto da classe Trem
+
+passageiro = Passageiro(cn, cs, screen) # Cria um objeto da classe Passageiro
+trem = Trem(cn, cs, screen) # Cria um objeto da classe Trem
 
 SCREEN_UPDATE = pygame.USEREVENT 
 pygame.time.set_timer(SCREEN_UPDATE, 150)
