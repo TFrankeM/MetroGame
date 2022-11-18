003400435c7a409155c323668f0db66364538f15
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-11-18 18:20:23 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,45 +3,49 @@ import pygame
 import random
 from pygame.math import Vector2
 sys.path.insert(0, '.')
-from classes import Passageiro, Trem
+from classes import Passageiro, Trem, Partida
 
 
-pygame.init()
-cs = 16 #cell_size
-cn = 40 #cell_number
-screen = pygame.display.set_mode((cn*cs,cn*cs)) # Tela do jogo
-pygame.display.set_caption("Metrô") # Adiciona um nome à janela
-clock = pygame.time.Clock()
+try:
+    pygame.init()
+    cn = 40 #cell_number
+    cs = 16 #cell_size
+    screen = pygame.display.set_mode((cn*cs,cn*cs)) # Tela do jogo
+    pygame.display.set_caption("Metrô") # Adiciona um nome à janela
+    clock = pygame.time.Clock()
 
-passageiro = Passageiro(cn, cs, screen) # Cria um objeto da classe Passageiro
-trem = Trem(cn, cs, screen) # Cria um objeto da classe Trem
 
-SCREEN_UPDATE = pygame.USEREVENT 
-pygame.time.set_timer(SCREEN_UPDATE, 150)
-# Criamos um evento que ocorre a cada 150 milissegundos e que vai acionar o movimento do trem
+    partida = Partida(cn, cs, screen)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-            # Identifica quando o usuário clica no x na janela, fechando o programa
-        if event.type == SCREEN_UPDATE:
-            trem.mover_trem()
-            # Identifica quando o evento periódico que foi criado fora do loop ocorre, chamando então a função mover_trem()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                trem.sentido = Vector2(0,-1)
-            if event.key == pygame.K_DOWN:
-                trem.sentido = Vector2(0,1)
-            if event.key == pygame.K_RIGHT:
-                trem.sentido = Vector2(1,0)
-            if event.key == pygame.K_LEFT:
-                trem.sentido = Vector2(-1,0)
-            # Identifica quando o usuário pressiona uma das setas do teclado e ajusta a direção do movimento do trem de acordo
-    
-    screen.fill((100,100,200)) # Preenche a tela com cor
-    passageiro.desenhar_passageiro() # Desenha o passageiro
-    trem.desenhar_trem() # Desenha o trem
-    pygame.display.flip() # Renderiza
-    clock.tick(60) # Garante uma frequência de cerca de 60 frames por segundo
+    SCREEN_UPDATE = pygame.USEREVENT 
+    pygame.time.set_timer(SCREEN_UPDATE, 150)
+    # Criamos um evento que ocorre a cada 150 milissegundos e que vai acionar o movimento do trem
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                # Identifica quando o usuário clica no x na janela, fechando o programa
+            if event.type == SCREEN_UPDATE:
+                partida.atualizar()
+                # Identifica quando o evento periódico que foi criado fora do loop ocorre, chamando então a função mover_trem()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP and partida.trem.sentido != Vector2(0,1):
+                    partida.trem.sentido = Vector2(0,-1)
+                if event.key == pygame.K_DOWN and partida.trem.sentido != Vector2(0,-1):
+                    partida.trem.sentido = Vector2(0,1)
+                if event.key == pygame.K_RIGHT and partida.trem.sentido != Vector2(-1, 0):
+                    partida.trem.sentido = Vector2(1,0)
+                if event.key == pygame.K_LEFT and partida.trem.sentido != Vector2(1, 0):
+                    partida.trem.sentido = Vector2(-1,0)
+                # Identifica quando o usuário pressiona uma das setas do teclado e ajusta a direção do movimento do trem de acordo
+        
+        screen.fill((100,100,200)) # Preenche a tela com cor
+        partida.desenhar_elementos()
+        pygame.display.flip() # Renderiza
+        clock.tick(60) # Garante uma frequência de cerca de 60 frames por segundo
+except:
+    print("Deu ruim!")
+    pygame.quit()
+    sys.exit()
