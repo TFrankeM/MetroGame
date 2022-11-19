6ff9cd3473d3c5afcc7b00623d46d65118e89057
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-11-19 14:44:35 -0300

    Update main.py
    Crie uma espera após se pressionar uma das setas do teclado

--- src/main.py
+++ src/main.py
@@ -4,11 +4,12 @@ import random
 from pygame.math import Vector2
 sys.path.insert(0, '.')
 from classes import Passageiro, Trem, Partida
+import time
 
 
 #try:
 pygame.init()
-cn = 30 #cell_number
+cn = 25 #cell_number
 cs = 32 #cell_size
 screen = pygame.display.set_mode((cn*cs,cn*cs)) # Tela do jogo
 pygame.display.set_caption("Metrô") # Adiciona um nome à janela
@@ -33,12 +34,16 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and partida.trem.sentido != Vector2(0,1):
                 partida.trem.sentido = Vector2(0,-1)
+                time.sleep(0.1) # Após se alterar o vetor de sentido do trem, uma breve espera foi adicionada para que o vetor não possa ser alterado novamente até a cobra se mover
             if event.key == pygame.K_DOWN and partida.trem.sentido != Vector2(0,-1):
                 partida.trem.sentido = Vector2(0,1)
+                time.sleep(0.1)
             if event.key == pygame.K_RIGHT and partida.trem.sentido != Vector2(-1, 0):
                 partida.trem.sentido = Vector2(1,0)
+                time.sleep(0.1)
             if event.key == pygame.K_LEFT and partida.trem.sentido != Vector2(1, 0):
                 partida.trem.sentido = Vector2(-1,0)
+                time.sleep(0.1)
             # Identifica quando o usuário pressiona uma das setas do teclado e ajusta a direção do movimento do trem de acordo
         
     screen.fill((100,100,200)) # Preenche a tela com cor
