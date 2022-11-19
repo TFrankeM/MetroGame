b610903435ab214a68b7152a1c6d86f608a019bb
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2022-11-19 15:23:11 -0300

    Update main.py
    Adicione pontuação e restart da partida após um game over

--- src/main.py
+++ src/main.py
@@ -14,9 +14,10 @@ cs = 32 #cell_size
 screen = pygame.display.set_mode((cn*cs,cn*cs)) # Tela do jogo
 pygame.display.set_caption("Metrô") # Adiciona um nome à janela
 clock = pygame.time.Clock()
+fonte = pygame.font.Font(None, 24)
 
 
-partida = Partida(cn, cs, screen)
+partida = Partida(cn, cs, screen, fonte)
 
 SCREEN_UPDATE = pygame.USEREVENT 
 pygame.time.set_timer(SCREEN_UPDATE, 150)
@@ -32,19 +33,17 @@ while True:
             partida.atualizar()
             # Identifica quando o evento periódico que foi criado fora do loop ocorre, chamando então a função mover_trem()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and partida.trem.sentido != Vector2(0,1):
+            if event.key == pygame.K_UP and partida.trem.corpo[1] != partida.trem.corpo[0]+Vector2(0,-1):
                 partida.trem.sentido = Vector2(0,-1)
-                time.sleep(0.1) # Após se alterar o vetor de sentido do trem, uma breve espera foi adicionada para que o vetor não possa ser alterado novamente até a cobra se mover
-            if event.key == pygame.K_DOWN and partida.trem.sentido != Vector2(0,-1):
+            if event.key == pygame.K_DOWN and partida.trem.corpo[1] != partida.trem.corpo[0]+Vector2(0,1):
                 partida.trem.sentido = Vector2(0,1)
-                time.sleep(0.1)
-            if event.key == pygame.K_RIGHT and partida.trem.sentido != Vector2(-1, 0):
+            if event.key == pygame.K_RIGHT and partida.trem.corpo[1] != partida.trem.corpo[0]+Vector2(1,0):
                 partida.trem.sentido = Vector2(1,0)
-                time.sleep(0.1)
-            if event.key == pygame.K_LEFT and partida.trem.sentido != Vector2(1, 0):
+            if event.key == pygame.K_LEFT and partida.trem.corpo[1] != partida.trem.corpo[0]+Vector2(-1,0):
                 partida.trem.sentido = Vector2(-1,0)
-                time.sleep(0.1)
             # Identifica quando o usuário pressiona uma das setas do teclado e ajusta a direção do movimento do trem de acordo
+            if partida.ativo == False:
+                partida = Partida(cn, cs, screen, fonte)
         
     screen.fill((100,100,200)) # Preenche a tela com cor
     menor = pygame.draw.rect(screen, (100,50,50), pygame.Rect(int(32), int(32), cs*(cn-2), cs*(cn-2)))
